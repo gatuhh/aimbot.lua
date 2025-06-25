e81d33b725e8f8d9eb62cffc4342be304be1e55e
gatuhh <davisamuel1253@gmail.com>
2025-06-25 09:16:17 -0300

    Update aimbot.lua

--- aimbot.lua
+++ aimbot.lua
@@ -1,20 +1,20 @@
 getgenv().Aimbot = {
     Settings = {
-        Prediction = 0.0440, 
-        AutoClick = false --
+        Prediction = 0.0440,
+        AutoClick = false -- Enables or disables autoclick
     },
     TargetLock = {
-        LockedTarget = nil, --
-        AimPart = "Head", 
-        Enabled = true 
+        LockedTarget = nil, -- Current target locked
+        AimPart = "Head",
+        Enabled = true
     },
     TargetStrafe = {
         Enabled = true,
-        UseButton = true,  
-        Distance = 5,  
-        Height = 5,    
-        Speed = 20     
-    },
+        UseButton = true,
+        Distance = 5,
+        Height = 5,
+        Speed = 20
+    }
 }
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/gatuhh/aimbot.lua/main/aimbot.lua"))()
