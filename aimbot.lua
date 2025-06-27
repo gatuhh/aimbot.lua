2732e4c2aecdb9742a0ebdbc39a664a4cc376d42
gatuhh <davisamuel1253@gmail.com>
2025-06-26 22:40:25 -0300

    aimbot.lua

---
+++ aimbot.lua
@@ -0,0 +1,17 @@
+getgenv().Settings = {
+    Prediction = 0.0440,
+    AutoClick = false,
+}
+
+getgenv().TargetLock = {
+    AimPart = "Head",
+    Enabled = true,
+}
+
+getgenv().TargetStrafe = {
+    Enabled = true,
+    UseButton = true,
+    Distance = 5,
+    Height = 5,
+    Speed = 20,
+}
